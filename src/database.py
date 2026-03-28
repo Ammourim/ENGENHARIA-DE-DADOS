bef363226ce76c86ed2c3f5faa9229d60067ad40
alan <alanamorim28@gmail.com>
2026-03-28 18:13:03 -0300

    fix: utilizando sintaxer do sqlalchemy no lugar do psycopg2

--- src/database.py
+++ src/database.py
@@ -1,94 +1,40 @@
-from sqlalchemy import create_engine
-import psycopg2
-from src.modelagem import df_pacientes, df_setores, df_atendimentos  # ou ajuste o import
+from sqlalchemy import text
+from src.database.connection import get_engine
+from src.modelagem import df_pacientes, df_setores, df_atendimentos
 
-def conexao():
-    engine = create_engine(
-        "postgresql+psycopg2://postgres:Ap200112.@localhost:5433/hospital"
-    )
-    return engine
+engine = get_engine()
 
-print("conectado com sucesso")
+print("Conectado com sucesso!")
 
-cursor = conexao().cursor()
+# -------------------------
+# CRIAR TABELAS
+# -------------------------
+with engine.begin() as conn:
+    conn.execute(text("""
+        CREATE TABLE IF NOT EXISTS pacientes (
+            id_paciente INT PRIMARY KEY,
+            paciente_nome TEXT
+        );
+    """))
 
-# Tabela pacientes
-cursor.execute("""
-CREATE TABLE IF NOT EXISTS pacientes (
-    id_paciente INT PRIMARY KEY,
-    paciente_nome TEXT
-);
-""")
+    conn.execute(text("""
+        CREATE TABLE IF NOT EXISTS setores (
+            id_setor INT PRIMARY KEY,
+            setor TEXT
+        );
+    """))
 
-# Tabela setores
-cursor.execute("""
-CREATE TABLE IF NOT EXISTS setores (
-    id_setor INT PRIMARY KEY,
-    setor TEXT
-);
-""")
-
-# Tabela atendimentos
-cursor.execute("""
-CREATE TABLE IF NOT EXISTS atendimentos (
-    id_atendimento INT PRIMARY KEY,
-    id_paciente INT,
-    id_setor INT,
-    tempo_espera_min FLOAT,
-    data_atendimento TIMESTAMP,
-    eh_outlier BOOLEAN,
-    FOREIGN KEY (id_paciente) REFERENCES pacientes(id_paciente),
-    FOREIGN KEY (id_setor) REFERENCES setores(id_setor)
-);
-""")
-
-conexao().commit()
+    conn.execute(text("""
+        CREATE TABLE IF NOT EXISTS atendimentos (
+            id_atendimento INT PRIMARY KEY,
+            id_paciente INT,
+            id_setor INT,
+            tempo_espera_min FLOAT,
+            data_atendimento TIMESTAMP,
+            eh_outlier BOOLEAN,
+            FOREIGN KEY (id_paciente) REFERENCES pacientes(id_paciente),
+            FOREIGN KEY (id_setor) REFERENCES setores(id_setor)
+        );
+    """))
 
 print("Tabelas criadas com sucesso!")
-
-
-for _, row in df_pacientes.iterrows():
-    cursor.execute("""
-        INSERT INTO pacientes (id_paciente, paciente_nome)
-        VALUES (%s, %s)
-        ON CONFLICT (id_paciente) DO NOTHING
-    """, (row['id_paciente'], row['paciente_nome']))
-
-conexao().commit()
-
-for _, row in df_setores.iterrows():
-    cursor.execute("""
-        INSERT INTO setores (id_setor, setor)
-        VALUES (%s, %s)
-        ON CONFLICT (id_setor) DO NOTHING
-    """, (row['id_setor'], row['setor']))
-
-conexao().commit()
-
-for _, row in df_atendimentos.iterrows():
-    cursor.execute("""
-        INSERT INTO atendimentos (
-            id_atendimento,
-            id_paciente,
-            id_setor,
-            tempo_espera_min,
-            data_atendimento,
-            eh_outlier
-        )
-        VALUES (%s, %s, %s, %s, %s, %s)
-        ON CONFLICT (id_atendimento) DO NOTHING
-    """, (
-        row['id_atendimento'],
-        row['id_paciente'],
-        row['id_setor'],
-        row['tempo_espera_min'],
-        row['data_atendimento'],
-        row['eh_outlier']
-    ))
-
-conexao().commit()
-
-print("Dados de pacientes, setores e atendimentos inseridos!")
-
-cursor.close()
-conexao().close()
